ef0097a90fe01c4590c8b8b64e4cd5aada6dfebc
Nivetha Kumar <88543014+Nivetha1298@users.noreply.github.com>
2022-08-24 18:06:01 +0530

    Add files via upload

--- server/src/pages/login/Login.tsx
+++ server/src/pages/login/Login.tsx
@@ -25,6 +25,9 @@ const navigate = useNavigate()
                 try{
 const  res = await axios.post("http://localhost:8005/api/auth/login" ,credentials)
 dispatch({type:"LOGIN_SUCCESS" ,  payload:res.data.details});
+   console.log( "Hello" ,res)
+   localStorage.setItem("isAdmin" ,res.data.isAdmin)
+   localStorage.setItem("acess_token" ,res.data.token)
 navigate("/")
 
                 }
